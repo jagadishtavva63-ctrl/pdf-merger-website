3feb426c91e770723fbe7bb9ae68a934145844c0
jagadishtavva63-ctrl <jagadishtavva63@gmail.com>
2026-01-23 02:39:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, request, send_file
 from PyPDF2 import PdfMerger
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 @app.route('/merge', methods=['POST'])
 def merge():
     files = request.files.getlist('pdfs')
+
+    if not files:
+        return "No files uploaded", 400
+
     merger = PdfMerger()
 
     for file in files:
@@ -22,5 +27,6 @@ def merge():
     return send_file(output, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
 
