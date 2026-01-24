af78f09f4b3c3f74353a2d98ecf6d8942eeaebbf
jagadishtavva63-ctrl <jagadishtavva63@gmail.com>
2026-01-24 12:21:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,14 @@ def merge():
     merger.close()
 
     return send_file(output, as_attachment=True)
+@app.route('/tools')
+def tools():
+    return render_template("tools.html")
+
+@app.route('/clipboard')
+def clipboard():
+    return render_template("clipboard.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
